aa917d53f1e0d22d72dc4adfe2dd404bc0cd26a4
abderrahim-alougane <74838790+abderrahim-alougane@users.noreply.github.com>
2021-07-18 21:48:17 +0100

    Add files via upload

---
+++ BLOG/Script.js
@@ -0,0 +1,52 @@
+// start animition
+// const PHAR= document.querySelector(".anim1");
+// text aninmition
+
+this.window.scrollY
+TweenMax.from(".anim1",4,{
+
+        delay:0,
+        opacity:0,
+        x:-300,
+        ease:Power4.easeOut
+    
+})
+
+// TweenMax.from(".anim1",4,{
+
+//     delay:0,
+//     opacity:0,
+//     x:-300,
+//     ease:Power4.easeOut
+
+// })
+TweenMax.from(".anim2",4,{
+
+    delay:0,
+    opacity:0,
+    x:300,
+    ease:Power4.easeOut
+
+})
+
+
+TweenMax.from(".anim5",4,{
+
+    delay:0,
+    opacity:0,
+    
+    y:-300, 
+    ease:Power4.easeOut
+
+})
+TweenMax.from(".anim6",4,{
+
+    delay:0,
+    opacity:0,
+    
+    y:300, 
+    ease:Power4.easeOut
+
+})
+
+
